316c7295a9879b44699c5b73a02e21f5be646405
dos <dos@dosowisko.net>
2008-12-28 17:31:03 +0000

    git-svn-id: svn://dosowisko.net/trunk@34 ca21fcdd-3915-4bab-9634-0d88803e2151

--- shr-settings/shr_usb.py
+++ shr-settings/shr_usb.py
@@ -3,21 +3,28 @@ import module, elementary
 class Usb(module.AbstractModule):
 
     def mode_handle(self, obj, event):
-        print obj.state_get()
+        if obj.state_get():
+            self.toggle1.label_set("Device mode:")
+            self.toggle1.states_labels_set("Ethernet","Mass storage")
+            self.toggle1.state_set(1)
+        else:
+            self.toggle1.label_set("Host powered:")
+            self.toggle1.states_labels_set("Yes","No")
+            self.toggle1.state_set(1)
 
     def name(self):
         return "USB"
 
     def view(self, win):
         box1 = elementary.Box(win)
-        toggle0 = elementary.Toggle(win)
-        toggle0.label_set("USB mode:")
-        toggle0.size_hint_align_set(-1.0, 0.0)
-        toggle0.states_labels_set("Device","Host")
-	toggle0.state_set(1)
-        toggle0.changed = self.mode_handle
-        box1.pack_start(toggle0)
-        toggle0.show()
+        self.toggle0 = elementary.Toggle(win)
+        self.toggle0.label_set("USB mode:")
+        self.toggle0.size_hint_align_set(-1.0, 0.0)
+        self.toggle0.states_labels_set("Device","Host")
+	self.toggle0.state_set(1)
+        self.toggle0.changed = self.mode_handle
+        box1.pack_start(self.toggle0)
+        self.toggle0.show()
 
 	self.toggle1 = elementary.Toggle(win)
 	self.toggle1.label_set("Device mode:")
