559df090f85b93e50aefbd9c652f69f9cf367adf
yoyo <yoyo>
2008-12-29 22:56:54 +0000

    git-svn-id: svn://dosowisko.net/trunk@57 ca21fcdd-3915-4bab-9634-0d88803e2151

--- shr-settings/shr_bt.py
+++ shr-settings/shr_bt.py
@@ -9,8 +9,9 @@ import module, elementary, os
 
 """
 btModels = [ \
-"/sys/devices/platform/s3c2410-i2c/i2c-adapter/i2c-0/0-0008/neo1973-pm-bt.0",
-"/sys/devices/platform/s3c2440-i2c/i2c-adapter/i2c-0/0-0008/neo1973-pm-bt.0"]
+"/sys/devices/platform/s3c2410-i2c/i2c-adapter/i2c-0/0-0008/neo1973-pm-bt.0/",
+"/sys/devices/platform/s3c2440-i2c/i2c-adapter/i2c-0/0-0008/neo1973-pm-bt.0/",
+"/sys/bus/platform/devices/neo1973-pm-bt.0"]
 
 class BtMstateContener:
     def __init__(self):
@@ -38,35 +39,42 @@ class BtMstateContener:
 
     def setPower(self, b ):
         print "BT BtMstateContener setPower [inf] "+self.model
-        if self.model=="gta01":
-            m=0
-        elif self.model=="gta02":
-            m=1
-        else:
-            return 0
-
         if b==0:
-            print "reset"
-            os.system("echo "+str(b)+" > "+btModels[m]+"/reset")
-            print "sleep"
+            print "stop /etc/init.d/bluetooth"
+            os.system("/etc/init.d/bluetooth stop")
             time.sleep(1)
+
+
+            print "reset"
+            os.system("echo "+str(b)+" > "+btModels[2]+"/reset")
+
+            print "sleep"
+            time.sleep(2)
+
             print "power_on"
-            os.system("echo "+str(b)+" > "+btModels[m]+"/power_on")
+            os.system("echo "+str(b)+" > "+btModels[2]+"/power_on")
         else:
             print "power_on"
-            os.system("echo "+str(b)+" > "+btModels[m]+"/power_on")
-            print "sleep"
+            os.system("echo "+str(b)+" > "+btModels[2]+"/power_on")
+
+            if self.model == "gta02":
+                print "sleep"
+                time.sleep(2)
+
+                print "reset"
+                os.system("echo "+str(b)+" > "+btModels[2]+"/reset")
+
             time.sleep(1)
-            print "reset"
-            os.system("echo "+str(b)+" > "+btModels[m]+"/reset")
+            print "start /etc/init.d/bluetooth"
+            os.system("/etc/init.d/bluetooth start")
 
         
 
     def getPower(self):
         if self.model=="gta01":
-            f0 = open(btModels[0]+"/power_on", "r")
+            f0 = open(btModels[2]+"/power_on", "r")
         elif self.model=="gta02":
-            f0 = open(btModels[1]+"/power_on", "r")
+            f0 = open(btModels[2]+"/power_on", "r")
         elif self.model=="":
             return self.state
 
@@ -105,14 +113,20 @@ class Bt(module.AbstractModule):
         
 
         box1 = elementary.Box(win)
-        self.toggle0 = elementary.Toggle(win)
-        self.toggle0.label_set("Bluetooth radio:")
-        self.toggle0.size_hint_align_set(-1.0, 0.0)
-        self.toggle0.states_labels_set("On","Off")
-        box1.pack_start(self.toggle0)
-        self.toggle0.show()
 
-        if self.btmc.getModel!="":
+        if self.btmc.getModel=="":
+            label =elementary.Label(win)
+            label.label_set("can't find file in sysfs")
+            label.toggle0.size_hint_align_set(-1.0, 0.0)
+            label.show()
+            box1.pack_start(label)
+        else:
+            self.toggle0 = elementary.Toggle(win)
+            self.toggle0.label_set("Bluetooth radio:")
+            self.toggle0.size_hint_align_set(-1.0, 0.0)
+            self.toggle0.states_labels_set("On","Off")
+            box1.pack_start(self.toggle0)
+            self.toggle0.show()
             self.toggle0.changed = self.toggle0Click
 
         self.BtmodGUIupdate()
