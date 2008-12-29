bbf84aee645faf8f1ea998c118d1cc50431a826a
dos <dos@dosowisko.net>
2008-12-29 00:40:36 +0000

    git-svn-id: svn://dosowisko.net/trunk@44 ca21fcdd-3915-4bab-9634-0d88803e2151

--- shr-settings/shr_clock.py
+++ shr-settings/shr_clock.py
@@ -1,6 +1,11 @@
-import module, elementary
+import module, elementary, os
 
 class Clock(module.AbstractModule):
+    def ntpsync(self, obj, event):
+	os.system("ntpdate ntp.org")
+        self.cl.edit_set(False)
+        self.but.label_set("Set time")
+        self.editable = False
     def clockset(self, obj, event):
 	if self.editable:
 	    self.cl.edit_set(False)
@@ -25,4 +30,10 @@ class Clock(module.AbstractModule):
 	box0.pack_end(self.but)
 	self.but.clicked = self.clockset
 	self.but.show()
+	ntp = elementary.Button(win)
+	ntp.label_set("Synchronize with ntp")
+	ntp.size_hint_align_set(-1.0, 0.0)
+	ntp.clicked = self.ntpsync
+	box0.pack_end(ntp)
+	ntp.show()
         return box0
