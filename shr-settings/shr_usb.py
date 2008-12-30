a910fd73a51557375a476e2788c41dfdb1807713
dos <dos@dosowisko.net>
2008-12-30 20:28:27 +0000

    bugfix on usb and wifi toggle is now implemented
    git-svn-id: svn://dosowisko.net/trunk@68 ca21fcdd-3915-4bab-9634-0d88803e2151

--- shr-settings/shr_usb.py
+++ shr-settings/shr_usb.py
@@ -8,8 +8,8 @@ class Usb(module.AbstractModule):
 
     def mode_handle(self, obj, event):
         #if obj.state_get():
-	btpower = self.usbhost.GetPower()
-        if btpower:
+	usbpower = self.usbhost.GetPower()
+        if usbpower:
             self.toggle1.label_set("Device mode:")
             self.toggle1.states_labels_set("Ethernet","Mass storage")
             self.toggle1.state_set(1)
@@ -19,7 +19,7 @@ class Usb(module.AbstractModule):
             self.toggle1.states_labels_set("Yes","No")
             self.toggle1.state_set(1)
 	    self.usbhost.SetPower(True)
-	obj.state_set(btpower)
+	obj.state_set(usbpower)
 
     def name(self):
         return "USB"
@@ -35,6 +35,8 @@ class Usb(module.AbstractModule):
         self.toggle0.states_labels_set("Device","Host")
 	self.toggle0.state_set(1)
         self.toggle0.changed = self.mode_handle
+	usbstate = self.usbhost.GetPower()
+	self.toggle0.state_set(not(usbstate))
         box1.pack_start(self.toggle0)
         self.toggle0.show()
 
@@ -45,6 +47,11 @@ class Usb(module.AbstractModule):
 	self.toggle1.state_set(1)
         box1.pack_end(self.toggle1)
         self.toggle1.show()
+
+	if usbstate:
+            self.toggle1.label_set("Host powered:")
+            self.toggle1.states_labels_set("Yes","No")
+            self.toggle1.state_set(1)
 
         return box1
         
--- shr-settings/shr_wifi.py
+++ shr-settings/shr_wifi.py
@@ -1,4 +1,8 @@
-import elementary, module, os
+import elementary, module, os, dbus
+
+def getDbusObject (bus, busname , objectpath , interface):
+        dbusObject = bus.get_object(busname, objectpath)
+        return dbus.Interface(dbusObject, dbus_interface=interface)
 
 class Wifi(module.AbstractModule):
     def name(self):
@@ -10,13 +14,25 @@ class Wifi(module.AbstractModule):
 	else:
 	    return 1
     
+    def power_handle(self, obj, event):
+        #if obj.state_get():
+        wifipower = self.wifi.GetPower()
+        self.wifi.SetPower(not(wifipower))
+        obj.state_set(not(wifipower))
+
+
     def view(self, win):
+        bus = dbus.SystemBus()
+        self.wifi = getDbusObject (bus, "org.freesmartphone.odeviced", "/org/freesmartphone/Device/PowerControl/WiFi", "org.freesmartphone.Device.PowerControl")
+
         box1 = elementary.Box(win)
         toggle0 = elementary.Toggle(win)
         toggle0.label_set("WiFi radio:")
         toggle0.size_hint_align_set(-1.0, 0.0)
         toggle0.states_labels_set("On","Off")
+	toggle0.changed = self.power_handle
         box1.pack_start(toggle0)
+	toggle0.state_set(self.wifi.GetPower())
         toggle0.show()
 
         return box1
