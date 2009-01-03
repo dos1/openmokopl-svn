69bf038af1872d947819e283772c1a02c5010cae
yoyo <yoyo>
2009-01-03 17:57:25 +0000

    git-svn-id: svn://dosowisko.net/trunk@101 ca21fcdd-3915-4bab-9634-0d88803e2151

--- shr-settings/shr_settings_modules/shr_profile.py
+++ shr-settings/shr_settings_modules/shr_profile.py
@@ -2,7 +2,14 @@ import elementary, module
 
 class Profile(module.AbstractModule):
     name = "Profile"
-    
+
+
+    def defbt_click(self, obj, event):
+        os.system("echo \"preferences.SetProfile('default')\" | cli-framework")
+
+    def silbt_click(self, obj, event):
+        os.system("echo \"preferences.SetProfile('silent')\" | cli-framework")
+
     def createView(self):
         boxh = elementary.Box(self.window)
         boxh.horizontal_set(True)
@@ -14,4 +21,19 @@ class Profile(module.AbstractModule):
         self.cur.label_set("default")
         boxh.pack_end(self.cur)
         self.cur.show()
+
+
+        defbt = elementary.Button(self.window)
+        defbt.clicked = self.defbt_click
+        defbt.label_set("default" )
+        defbt.size_hint_align_set(-1.0, 0.0)
+        boxh.pack_end(defbt)
+
+        silbt = elementary.Button(self.window)
+        silbt.clicked = self.silbt_click
+        silbt.label_set("silent" )
+        silbt.size_hint_align_set(-1.0, 0.0)
+        boxh.pack_end(silbt)
+
+
         return boxh
