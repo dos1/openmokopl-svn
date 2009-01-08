fd88845186c35ca4a42747dd7d926b4b32e81115
yoyo <yoyo>
2009-01-08 20:13:30 +0000

    for fun
    git-svn-id: svn://dosowisko.net/trunk@131 ca21fcdd-3915-4bab-9634-0d88803e2151

--- shr-settings/shr_settings_modules/shr_sim.py
+++ shr-settings/shr_settings_modules/shr_sim.py
@@ -49,6 +49,22 @@ class SimMstateContener:
         else:
             return self.gsm_sim_iface.GetPhonebookInfo(a)
 
+
+    def GetMessagebookInfo(self):
+        if self.state == 0:
+            return 0
+        else:
+            return self.gsm_sim_iface.GetMessagebookInfo()
+
+
+    def MessageBookClean(self):
+        messageMax = self.GetMessagebookInfo()['last']
+        print "MessageBookClean max"+str(messageMax)
+        for i in range(1, (messageMax+1), 1):
+            print "remove id:"+str(i)
+            self.gsm_sim_iface.DeleteMessage(i)
+        print "DONE"
+
     def getService(self):
         ser = os.popen("ps -A | grep hci").read().replace("\n","")
         if ser != "":
@@ -247,6 +263,9 @@ class Sim(module.AbstractModule):
             print "Turn on"
             os.system("cd /tmp && obexftpd -b -c /tmp &")
 
+    def cleanMessageBookClick(self, obj, event):
+        self.simmc.MessageBookClean()
+
 
     def createView(self):
         self.guiUpdate = 1
@@ -311,8 +330,11 @@ class Sim(module.AbstractModule):
                 boxBook.show()
                 frameBook.content_set(boxBook)
 
+
+                print "phoneBookInfo: "+b
                 phoneBookInfo = self.simmc.GetPhonebookInfo( b )
                 for i in phoneBookInfo:
+                    print "phoneBookInfo: "+b+"; "+i
                     #info state
                     boxS = elementary.Box(self.window)
                     boxS.horizontal_set(True)
@@ -320,7 +342,10 @@ class Sim(module.AbstractModule):
                     boxS.show()
 
                     labelN =elementary.Label(self.window)
-                    labelN.label_set(str(i)+":")
+                    try:
+                        labelN.label_set(str(i)+":")
+                    except:
+                        pass
                     labelN.size_hint_align_set(-1.0, -1.0)
                     labelN.size_hint_weight_set(1.0, 1.0)
                     labelN.show()
@@ -328,38 +353,106 @@ class Sim(module.AbstractModule):
 
                     labelV =elementary.Label(self.window)
                     labelV.size_hint_align_set(-1.0, 0.0)
-                    labelV.label_set( str( phoneBookInfo[i] ) )
+                    try:
+                        labelV.label_set( str( phoneBookInfo[i] ) )
+                    except:
+                        pass
                     labelV.show()
                     boxS.pack_end(labelV)
 
                     boxBook.pack_start( boxS )
 
+            print "phoneBookInfo --------- DONE"
 
+            """
+            # actions
+            boxS = elementary.Box(self.window)
+            boxS.horizontal_set(True)
+            boxS.size_hint_align_set(-1.0, 0.0)
+            boxS.show()
 
-                # actions
+            # backup TODO
+            backupbt = elementary.Button(self.window)
+            #backupbt.clicked = self.destroy2
+            backupbt.label_set("backup")
+            backupbt.size_hint_align_set(-1.0, 0.0)
+            backupbt.show()
+            boxS.pack_end(backupbt)
+
+            # clear TODO
+            cleanbt = elementary.Button(self.window)
+            #backupbt.clicked = self.destroy2
+            cleanbt.label_set("clean")
+            cleanbt.size_hint_align_set(-1.0, 0.0)
+            cleanbt.show()
+            boxS.pack_end(cleanbt)
+
+            boxBook.pack_end( boxS )
+            """
+            print "1"
+            # message book info
+
+            messBookInfo = self.simmc.GetMessagebookInfo()
+            print "2"
+            frameBook = elementary.Frame(self.window)
+            print "3"
+            frameBook.label_set("Message book:")
+            box1.pack_end(frameBook)
+            frameBook.size_hint_align_set(-1.0, 0.0)
+            frameBook.show()
+
+            boxBook = elementary.Box(self.window)
+            boxBook.show()
+            frameBook.content_set(boxBook)
+            
+            for m in messBookInfo:
                 boxS = elementary.Box(self.window)
                 boxS.horizontal_set(True)
                 boxS.size_hint_align_set(-1.0, 0.0)
                 boxS.show()
 
-                # backup TODO
-                backupbt = elementary.Button(self.window)
-                #backupbt.clicked = self.destroy2
-                backupbt.label_set("backup")
-                backupbt.size_hint_align_set(-1.0, 0.0)
-                backupbt.show()
-                boxS.pack_end(backupbt)
+                labelN =elementary.Label(self.window)
+                labelN.label_set(str(m)+":")
+                labelN.size_hint_align_set(-1.0, -1.0)
+                labelN.size_hint_weight_set(1.0, 1.0)
+                labelN.show()
+                boxS.pack_start(labelN)
 
-                # clear TODO
-                cleanbt = elementary.Button(self.window)
-                #backupbt.clicked = self.destroy2
-                cleanbt.label_set("clean")
-                cleanbt.size_hint_align_set(-1.0, 0.0)
-                cleanbt.show()
-                boxS.pack_end(cleanbt)
+                labelV =elementary.Label(self.window)
+                labelV.size_hint_align_set(-1.0, 0.0)
+                labelV.label_set( str( messBookInfo[m] ) )
+                labelV.show()
+                boxS.pack_end(labelV)
 
-                boxBook.pack_end( boxS )
+                boxBook.pack_start( boxS )
 
+
+            # actions
+            boxS = elementary.Box(self.window)
+            boxS.horizontal_set(True)
+            boxS.size_hint_align_set(-1.0, 0.0)
+            boxS.show()
+            """
+            # backup TODO
+            backupbt = elementary.Button(self.window)
+            #backupbt.clicked = self.destroy2
+            backupbt.label_set("backup")
+            backupbt.size_hint_align_set(-1.0, 0.0)
+            backupbt.show()
+            boxS.pack_end(backupbt)
+            """
+            # clear TODO
+            cleanbt = elementary.Button(self.window)
+            cleanbt.clicked = self.cleanMessageBookClick
+            cleanbt.label_set("clean")
+            cleanbt.size_hint_align_set(-1.0, 0.0)
+            cleanbt.show()
+            boxS.pack_end(cleanbt)
+
+            boxBook.pack_end( boxS )
+            
+
+            
 
             """
             self.toggle0 = elementary.Toggle(self.window)
