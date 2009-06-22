63d2192e6275dc9789c377f93fcb25439d92e2ba
dos <dos@dosowisko.net>
2009-06-22 23:09:11 +0000

    opimd: again many changes :)
    git-svn-id: svn://dosowisko.net/trunk@192 ca21fcdd-3915-4bab-9634-0d88803e2151

--- opimd/opimd_utils/opimd_reply.py
+++ opimd/opimd_utils/opimd_reply.py
@@ -13,33 +13,109 @@ def dbus_opimd_ok(func, x):
   if callable(func):
     func(x)
 
+def retry_msg(to, text, bus, win, inwin, func_ok, func_err, *args, **kwargs ):
+  inwindelete(inwin)
+  reply(to, text, bus, win, func_ok, func_err)
+
+def dbus_gsm_err(to, text, bus, win, func_ok, func_err, x):
+  inwin = elementary.InnerWindow(win)
+  inwin.show()
+  inwin.style_set("vertical-minimal")
+  win.resize_object_add(inwin)
+  box = elementary.Box(inwin)
+  box.show()
+  inwin.content_set(box)
+  label = elementary.Label(inwin)
+  label.label_set("Error while sending message!")
+  label.show()
+  box.pack_start(label)
+  sc = elementary.Scroller(inwin)
+  sc.bounce_set(0, 0)
+  sc.show()
+  anchor = elementary.AnchorBlock(inwin)
+  anchor.text_set(str(x))
+  anchor.show()
+  sc.content_set(anchor)
+  sc.size_hint_weight_set(1.0, 1.0)
+  sc.size_hint_align_set(-1.0, -1.0)
+  box.pack_end(sc)
+
+  hbox = elementary.Box(inwin)
+  hbox.horizontal_set(1)
+  hbox.show()
+
+  retry = elementary.Button(inwin)
+  retry.label_set("Retry")
+  retry.show()
+  retry.clicked = partial(retry_msg, to, text, bus, win, inwin, func_ok, func_err)
+  hbox.pack_end(retry)
+
+  hide = elementary.Button(inwin)
+  hide.label_set("Close")
+  hide.show()
+  hide.clicked = partial(inwindelete, inwin)
+  hbox.pack_end(hide)  
+
+  box.pack_end(hbox)
+
+  if callable(func_err):
+    func_err()
+
+  inwin.activate()
+
 def dbus_err(x):
   print "dbus error! "+str(x)
 
-def send_msg(bus, to, entry, inwin, func, *args, **kwargs):
+def send_msg(to, entry, bus, inwin, win, func_ok, func_err, *args, **kwargs):
   ogsmd = getDbusObject (bus, "org.freesmartphone.ogsmd", "/org/freesmartphone/GSM/Device", "org.freesmartphone.GSM.SMS")
+
   msg = entry.entry_get().replace('<br>','')
-  ogsmd.SendMessage(to[0].replace('tel:','') ,msg, {}, reply_handler=dbus_sms_ok, error_handler=dbus_err)
+  ogsmd.SendMessage(to[0].replace('tel:','') ,msg, {}, reply_handler=dbus_sms_ok, error_handler=partial(dbus_gsm_err, to, msg, bus, win, func_ok, func_err) )
+
   messages = getDbusObject (bus, "org.freesmartphone.opimd", "/org/freesmartphone/PIM/Messages", "org.freesmartphone.PIM.Messages")
-  messages.Add({'Recipient':to[0],'Direction':'out','Folder':'SMS','Text':msg}, reply_handler=partial(dbus_opimd_ok, func), error_handler=dbus_err)
+  messages.Add({'Recipient':to[0],'Direction':'out','Folder':'SMS','Text':msg}, reply_handler=partial(dbus_opimd_ok, func_ok), error_handler=dbus_err)
+
   inwin.delete()
 
 def inwindelete(win, *args, **kwargs):
   win.delete()
 
-def reply(bus, to, win, func, *args, **kwargs):
+def update_chars(label, obj, event, *args, **kwargs):
+  label.label_set("(%d)" % len(obj.entry_get().replace('<br>','')))
+
+def reply(to, text, bus, win, func_ok, func_err, *args, **kwargs):
   inwin = elementary.InnerWindow(win)
   win.resize_object_add(inwin)
   inwin.show()
 #  inwin.style_set("minimal")
   box = elementary.Box(inwin)
   box.show()
+
+  tbox = elementary.Box(inwin)
+  tbox.show()
+  tbox.horizontal_set(1)  
+
   label = elementary.Label(inwin)
   label.label_set("To: "+to[1])
   label.show()
-  box.pack_start(label)
+  tbox.pack_start(label)
+
+  chars = elementary.Label(inwin)
+  chars.label_set("(%d)" % len(text))
+  chars.show()
+  tbox.pack_end(chars)
+
+  tbox.size_hint_weight_set(-1.0, 0.0)
+  tbox.size_hint_align_set(-1.0, -1.0)
+
+  chars.size_hint_weight_set(1.0, 0.0)
+  chars.size_hint_align_set(1.0, -1.0)
+
+  box.pack_start(tbox)
   scroll = elementary.Scroller(inwin)
   entry = elementary.Entry(inwin)
+  entry.entry_set(text)
+  entry.changed = partial(update_chars, chars)
   entry.size_hint_weight_set(1.0, 1.0)
   entry.size_hint_align_set(-1.0, -1.0)
   entry.show()
@@ -62,9 +138,9 @@ def reply(bus, to, win, func, *args, **kwargs):
   send = elementary.Button(inwin)
   send.label_set("Send")
   send.show()
-  send.clicked = partial(send_msg, bus, to, entry, inwin, func)
+  send.clicked = partial(send_msg, to, entry, bus, inwin, win, func_ok, func_err)
 
-  hbox.pack_end(send)
+  hbox.pack_start(send)
 
   box.pack_end(hbox)
 
