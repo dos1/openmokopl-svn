bcee7c98fb5c0ef034f9e227510587941e420a39
dos <dos@dosowisko.net>
2009-07-02 16:48:11 +0000

    opimd: implement options (now class 0 and delivery reports)
    git-svn-id: svn://dosowisko.net/trunk@208 ca21fcdd-3915-4bab-9634-0d88803e2151

--- opimd/opimd_utils/opimd_reply.py
+++ opimd/opimd_utils/opimd_reply.py
@@ -1,6 +1,25 @@
 
 import elementary, dbus
+import ConfigParser
 from functools import partial
+
+CONF_FILE = '/etc/opimd-utils.conf'
+
+opts = {
+    'report':'request delivery report',
+#    'charset':'force GSM ASCII charset',
+#    'store':'store SMS in outbox',
+    'class':'send as Class 0',
+#    'askcsm':'ask before sending CSM',
+  }
+
+optsdef = {
+    'report':True,
+    'charset':False,
+    'store':True,
+    'class':False,
+    'askcsm':True,
+  }
 
 def getDbusObject (bus, busname , objectpath , interface):
         dbusObject = bus.get_object(busname, objectpath)
@@ -88,8 +107,12 @@ def dbus_opimd_err(to, msg, props, bus, win, func_ok, func_err, x):
 
 def send_msg(to, entry, bus, inwin, win, func_ok, func_err, *args, **kwargs):
   msg = entry.markup_to_utf8(entry.entry_get())
-  props = {'status-report-request':1}
-#  props = {}
+  ops = loadOpts()
+  props = {}
+  if ops['report']:
+    props['status-report-request']=1
+  if ops['class']:
+    props['message-class']=0
 
 #  ogsmd.SendMessage(to[0].replace('tel:','') ,msg, props, reply_handler=dbus_sms_ok, error_handler=partial(dbus_gsm_err, to, msg, bus, win, func_ok, func_err) )
 
@@ -106,7 +129,133 @@ def send_msg(to, entry, bus, inwin, win, func_ok, func_err, *args, **kwargs):
 def inwindelete(win, *args, **kwargs):
   win.delete()
 
-def update_chars(label, obj, event, *args, **kwargs):
+def saveOpts(optsval):
+  parser = ConfigParser.SafeConfigParser()
+  parser.read(CONF_FILE)
+  section = 'SMS'
+  for opt in optsval:
+    key = opt
+    value = optsval[opt].state_get()
+    try:
+      parser.set( section, key, str(value) )
+    except ConfigParser.NoSectionError:
+      parser.add_section( section )
+      parser.set( section, key, str(value) )
+  parser.write(open(CONF_FILE, 'w'))
+
+def loadOpts():
+#  return {'charset':True} # FIXME
+  parser = ConfigParser.SafeConfigParser()
+  parser.read(CONF_FILE) 
+  try:
+    options = parser.options( 'SMS' )
+  except ConfigParser.NoSectionError:
+    return {}
+  else:
+    ops = {}
+    for opt in options:
+      try:
+        ops[opt] = parser.getboolean('SMS', opt )
+      except:
+        ops[opt] = optsdef[opt]
+
+    return ops
+
+
+def hide_opts(pager, optsval, *args, **kwargs):
+  saveOpts(optsval)
+  pager.content_pop()
+
+def show_opts(pager, *args, **kwargs):
+  box = elementary.Box(pager)
+  box.show()
+
+  scroller = elementary.Scroller(pager)
+  scroller.show()
+  scroller.bounce_set(0, 0)
+  scroller.size_hint_weight_set(1.0, 1.0)
+  scroller.size_hint_align_set(-1.0, -1.0)
+  box.pack_start(scroller)
+
+  oframe = elementary.Frame(pager)
+  oframe.label_set("Options")
+  oframe.show()
+  oframe.style_set('pad_small')
+  oframe.size_hint_weight_set(1.0, 1.0)
+  oframe.size_hint_align_set(-1.0, -1.0)
+
+
+  obox = elementary.Box(oframe)
+  obox.show()
+
+  optsconf = loadOpts()
+
+  optsval = {}
+
+  """
+  report = elementary.Check(obox)
+  report.label_set('request delivery report')
+  report.show()
+  report.size_hint_weight_set(1.0, 0.0)
+  report.size_hint_align_set(0.0, 0.0)
+  obox.pack_end(report)
+
+  charset = elementary.Check(obox)
+  charset.label_set('force GSM ASCII charset')
+  charset.show()
+  charset.size_hint_weight_set(-1.0, 0.0)
+  charset.size_hint_align_set(-1.0, 0.0)
+  obox.pack_end(charset)
+
+  store = elementary.Check(obox)
+  store.label_set('store SMS in outbox')
+  store.show()
+  store.size_hint_weight_set(-1.0, 0.0)
+  store.size_hint_align_set(-1.0, 0.0)
+  obox.pack_end(store)
+
+  flash = elementary.Check(obox)
+  flash.label_set('send as Class 0')
+  flash.show()
+  flash.size_hint_weight_set(-1.0, 0.0)
+  flash.size_hint_align_set(-1.0, 0.0)
+  obox.pack_end(flash)
+
+  csm = elementary.Check(obox)
+  csm.label_set('ask before sending CSM')
+  csm.show()
+  csm.size_hint_weight_set(-1.0, 0.0)
+  csm.size_hint_align_set(-1.0, 0.0)
+  obox.pack_end(csm)
+  """
+
+  for opt in opts:
+    chk = elementary.Check(obox)
+    chk.label_set(opts[opt])
+    chk.show()
+    chk.size_hint_weight_set(-1.0, 0.0)
+    chk.size_hint_align_set(-1.0, 0.0)
+    obox.pack_end(chk)
+    try:
+      chk.state_set(optsconf[opt])
+    except:
+      chk.state_set(optsdef[opt])
+    optsval[opt] = chk
+
+  button = elementary.Button(pager)
+  button.label_set('Back')
+  button.show()
+  button.clicked = partial(hide_opts, pager, optsval)
+  button.size_hint_weight_set(-1.0, 0.0)
+  button.size_hint_align_set(-1.0, 0.0)
+  box.pack_end(button)
+
+  oframe.content_set(obox)
+  scroller.content_set(oframe)
+
+  pager.content_push(box)
+
+def update_chars(label, obj, *args, **kwargs):
   label.label_set("(%d)" % len(obj.markup_to_utf8(obj.entry_get())))
 
 def reply(to, text, bus, win, func_ok, func_err, *args, **kwargs):
@@ -114,6 +263,11 @@ def reply(to, text, bus, win, func_ok, func_err, *args, **kwargs):
   win.resize_object_add(inwin)
   inwin.show()
 #  inwin.style_set("minimal")
+
+  pager = elementary.Pager(inwin)
+  inwin.content_set(pager)
+  pager.show()
+
   box = elementary.Box(inwin)
   box.show()
 
@@ -142,7 +296,7 @@ def reply(to, text, bus, win, func_ok, func_err, *args, **kwargs):
   entry = elementary.Entry(inwin)
   if text:
     entry.entry_set(entry.utf8_to_markup(text))
-  entry.changed = partial(update_chars, chars)
+  entry.changed = partial(update_chars, chars, entry)
   entry.size_hint_weight_set(1.0, 1.0)
   entry.size_hint_align_set(-1.0, -1.0)
   entry.show()
@@ -155,6 +309,12 @@ def reply(to, text, bus, win, func_ok, func_err, *args, **kwargs):
   hbox = elementary.Box(win)
   hbox.horizontal_set(1)
   hbox.show()
+
+  opts = elementary.Button(inwin)
+  opts.label_set("Options")
+  opts.show()
+  opts.clicked = partial(show_opts, pager)
+  hbox.pack_end(opts)
 
   hide = elementary.Button(inwin)
   hide.label_set("Close")
@@ -174,7 +334,8 @@ def reply(to, text, bus, win, func_ok, func_err, *args, **kwargs):
   box.size_hint_weight_set(-1.0, -1.0)
   box.size_hint_align_set(-1.0, -1.0)
 
-  inwin.content_set(box)
+#  inwin.content_set(box)
+  pager.content_push(box)
   box.show()
   inwin.activate()
 
